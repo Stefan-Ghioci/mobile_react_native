1c9bb293366782b7c27f4bc71116d376ae178b5b
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-12-28 14:03:08 +0200

    Migrate from Expo to React Native CLI

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
 import AppContainer from './nav';
 
 // fetch('http://192.168.0.102:8080/mobileapps/games', {
