6d3850577ecffe132e838989806f1225117873ed
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-12-28 17:35:31 +0200

    Fetch data from server

--- screens/home/HomeScreenContainer.js
+++ screens/home/HomeScreenContainer.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import React, { useEffect, useState } from 'react';
+import { ScrollView } from 'react-native';
+import { Image, ListItem, Rating } from 'react-native-elements';
 
-// fetch('http://192.168.0.102:8080/mobileapps/games', {
-//   method: 'GET'
-// })
-//   .then(response => response.json())
-//   .then(responseJSON => console.log(responseJSON))
-//   .catch(error => console.log(error));
 const HomeScreenContainer = () => {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    fetch('http://192.168.0.102:8080/mobileapps/games', {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(responseJson => setGames(responseJson))
+      .catch(error => console.log(error));
+  }, []);
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Home</Text>
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      {games.map((e, i) => (
+        <>
+          <ListItem key={i} title={e.name} subtitle={e.date} />
+          <Rating showRating fractions='1' readOnly startingValue={e.rating} />
+          <Image
+            source={{ uri: e.imageURL }}
+            style={{ width: '100%', height: 100 }}
+          />
+        </>
+      ))}
+    </ScrollView>
   );
 };
 
